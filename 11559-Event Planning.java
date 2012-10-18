bffa933ad59f52297d612013eaed4d1fd0a53329
gsphanikumar <gsphanikumar@gmail.com>
2012-10-18 21:53:24 +0530

    Event Planning Event Planning

---
+++ 11559-Event Planning.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class Main {
+    public static void main(String args[] ) throws Exception {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        while (true) {
+            str = in.readLine();
+            if(str==null || str.isEmpty()) break;
+            String[] temp = str.split(" ");
+            int no_of_par = Integer.parseInt(temp[0]);
+            int bud = Integer.parseInt(temp[1]);
+            int hotels =Integer.parseInt(temp[2]);
+            int weeks = Integer.parseInt(temp[3]);
+            int[] result = new int[hotels];
+            int count = 0;
+            for(int i=0;i<hotels;i++)
+                  {
+                    int price_for_one = Integer.parseInt(in.readLine());
+                    String[] beds = in.readLine().split(" ");
+                    boolean valid = false;
+                    for(int j=0;j<weeks;j++)
+                        {
+                            if (Integer.parseInt(beds[j]) >= no_of_par )
+                                {
+                                    if(no_of_par * price_for_one <= bud)
+                                    {
+                                       result[count] = no_of_par * price_for_one;
+                                        count++;
+                                       break;
+                                    }
+                                }
+                        }
+                  }
+            int res =result[0];
+            if (count > 0)  {
+               for(int i=0;i<count;i++)
+                  res = Math.min(res,result[i]);
+               System.out.println(res);
+            }
+            else
+                System.out.println("stay home");
+            }
+        }
+}
+
